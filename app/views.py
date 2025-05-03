20c94c3fc3c874fe176ef1d4467f6f1b0bbc830d
MyhaylovTymur <t0993321550@gmail.com>
2025-05-03 11:48:55 +0000

    added image and text generation

--- app/views.py
+++ app/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from .forms import PromptForm
+from project.settings import API_KEY
 
-# Create your views here.
+import openai
+# openai.api_key = ''
+client = openai.OpenAI(api_key=API_KEY)
+
+def home(request):
+    response_text = None
+    response_image = None
+
+    if request.method == 'POST':
+        form = PromptForm(request.POST)
+        # is_valid
+        if form.is_valid():
+            prompt = form.cleaned_data['prompt']
+
+            completion = client.chat.completions.create(
+                model="gpt-4.1",
+                messages=[
+                    {"role": "developer", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt}
+                ]
+            )
+
+            response_text = completion.choices[0].message.content
+
+#            generate image 
+#            image_result = client.images.generate(
+#                model="dall-e-3",
+#                prompt=prompt
+#                )
+#            response_image = image_result.data[0].url
+
+            # model "gpt-image-1"
+            image_result = client.images.generate(
+                model="gpt-image-1",
+                prompt=prompt
+            )
+
+            image_base64 = image_result.data[0].b64_json
+            response_image = image_base64.b64decode(image_base64)
+
+    else:
+        form = PromptForm()
+
+    return render(request, 'home.html', {
+        'form': form,
+        'response_text': response_text,
+        'response_image': response_image
+
+    })
--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
